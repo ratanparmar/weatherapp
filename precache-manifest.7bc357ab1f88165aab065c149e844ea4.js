58c2094826e532ecbdaeee005a8dd62da8925c7c
ratanparmar <parmar.ratan@gmail.com>
2020-08-03 04:39:50 +0530

    Updates

---
+++ precache-manifest.7bc357ab1f88165aab065c149e844ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772194c6f58dd5631bca27323164c14d",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "8b80335aab73de2785c7",
+    "url": "/weatherapp/static/css/main.11665d27.chunk.css"
+  },
+  {
+    "revision": "79139980918bcb7c9da1",
+    "url": "/weatherapp/static/js/2.c981e34a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weatherapp/static/js/2.c981e34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b80335aab73de2785c7",
+    "url": "/weatherapp/static/js/main.195fdec9.chunk.js"
+  },
+  {
+    "revision": "b8dfc0a02113a5d973a4",
+    "url": "/weatherapp/static/js/runtime-main.3332107b.js"
+  }
+]);

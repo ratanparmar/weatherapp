eb356b84b5e0547dfdbcfa9d309dd34239b19f3d
Ratan Parmar <macbook@macbooks-MacBook-Pro.local>
2021-07-14 17:23:43 +0530

    build added

---
+++ build/precache-manifest.d0a3e6d37b47c5e9dda52a808800ef40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c52d8d9e448835f182c57721837721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b0b14353e18eb346fb",
+    "url": "/static/css/main.11665d27.chunk.css"
+  },
+  {
+    "revision": "79139980918bcb7c9da1",
+    "url": "/static/js/2.c981e34a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c981e34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b0b14353e18eb346fb",
+    "url": "/static/js/main.d26d1857.chunk.js"
+  },
+  {
+    "revision": "7e6045739f1df821f3db",
+    "url": "/static/js/runtime-main.5e7e2746.js"
+  }
+]);
